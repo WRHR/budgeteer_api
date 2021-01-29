c4f967cba10c3d0705efea8a0bc74033018023f8
WRHR <wrhreeves@gmail.com>
2021-01-28 20:52:21 -0700

    Create income entity

---
+++ src/entities/Income.ts
@@ -0,0 +1,14 @@
+import { Field, ObjectType } from 'type-graphql'
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@ObjectType()
+@Entity()
+export class Income extends BaseEntity{
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!:number
+
+  @Field()
+  @Column()
+  amount!:number
+}

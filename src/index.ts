471cf3074260b3133b0834d4a734f079d9dff1d3
WRHR <wrhreeves@gmail.com>
2021-01-28 20:20:45 -0700

    Set up scripts and server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata'
+import express from "express";
+import dotenv from "dotenv";
+
+const main = async () => {
+  dotenv.config();
+
+  const app = express();
+
+  const PORT = process.env.PORT;
+
+  app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+  });
+};
+
+main();
